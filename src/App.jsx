dfbfdb16374b453fcf8e9627898f4e25dc7d58ab
Ruivon <paulooliveira5348@gmail.com>
2025-10-01 23:14:43 -0300

    Algumas mudanças de Grid e cores mais as fontes.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <main className="dashboard">
         {/* Bloco 1: Superior Esquerdo (com ícone) */}
         <div className="card card-top-left">
+          <h2>Fono Noemi de Oliveira</h2>
           <svg className="sun-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.106a.75.75 0 011.06-1.06l1.592 1.591a.75.75 0 11-1.06 1.06l-1.592-1.591zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.894 17.894a.75.75 0 011.06 1.06l-1.592 1.592a.75.75 0 01-1.06-1.061l1.592-1.591zM12 18.75a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0v-2.25a.75.75 0 01.75-.75zM6.106 17.894a.75.75 0 011.06-1.06l1.592 1.591a.75.75 0 11-1.06 1.06l-1.592-1.591zM3 12a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm3.106-4.894a.75.75 0 011.06 1.06L5.606 9.75a.75.75 0 01-1.06-1.06l1.56-1.56z" />
           </svg>
@@ -16,7 +17,8 @@ function App() {
         <div className="card card-top-middle"></div>
 
         {/* Bloco 3: Esquerdo Inferior */}
-        <div className="card card-bottom-left"></div>
+        <div className="card card-bottom-left">
+        </div>
 
         {/* Bloco 4: Meio Inferior (cor clara) */}
         <div className="card card-bottom-middle card-light"></div>
